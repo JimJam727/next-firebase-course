e8add5b794e83415082a64b5260d6176ea97cb68
Jimmy Christensen'git config --global user.name Jimmy <jimmyc0727@gmail.com>
2021-04-23 15:18:48 -0500

    Prepare for Slug Bug Inspection

---
+++ lib/firebase.ts
@@ -0,0 +1,53 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBU83asWHsTHpnSYdh7TwqgrBtP8Dtn63M",
+  authDomain: "next-js-tutorial-3b39b.firebaseapp.com",
+  projectId: "next-js-tutorial-3b39b",
+  storageBucket: "next-js-tutorial-3b39b.appspot.com",
+  messagingSenderId: "677474900784",
+  appId: "1:677474900784:web:ff66c4e4f1d700193206f5",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
+
+export const fromMillis = firebase.firestore.Timestamp.fromMillis;
+
+/// Helper functions
+
+/**`
+ * Gets a users/{uid} document with username
+ * @param {string} username
+ */
+export async function getUserWithUsername(username) {
+  const usersRef = firestore.collection("users");
+  const query = usersRef.where("username", "==", username).limit(1);
+  const userDoc = (await query.get()).docs[0];
+  return userDoc;
+}
+
+/**
+ * Converts a firestore document to JSON
+ * @param {DocumentSnapshot} doc
+ */
+
+export function postToJSON(doc) {
+  const data = doc.data();
+  return {
+    ...data,
+    //Gotcha! firestore timestamp NOT serializable to JSON
+    createdAt: data?.createdAt.toMillis(),
+    updatedAt: data?.updatedAt.toMillis(),
+  };
+}
---
+++ pages/admin/[slug].tsx
@@ -0,0 +1,9 @@
+import Metatags from "@components/Metatags";
+
+export default function AdminPostEdit({}) {
+  return (
+    <main>
+      <h1>Edit Post</h1>
+    </main>
+  );
+}
